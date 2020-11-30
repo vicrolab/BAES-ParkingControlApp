73564aba261a62fea6cef8d1cfcb5804b1cadb13
Mikalaj <shuhno@yahoo.com>
2020-11-30 19:13:06 +0300

    first commit

---
+++ ParkingControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParkingControl
+//
+//  Created by Mikalaj Shuhno on 30.11.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
