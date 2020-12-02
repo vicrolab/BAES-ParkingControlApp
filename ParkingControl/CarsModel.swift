85ff528a72601a37e700e9fd30d8b5b93ff9ac5a
Mikalaj <shuhno@yahoo.com>
2020-12-02 12:23:25 +0300

    add UI to fixVehicleTableViewController

---
+++ ParkingControl/CarsModel.swift
@@ -0,0 +1,16 @@
+//
+//  CarsModel.swift
+//  ParkingControl
+//
+//  Created by Mikalaj Shuhno on 2.12.20.
+//
+
+import Foundation
+
+struct CarModels {
+    var CarModel = ["Audi": ["A1","A2", "A3", "A4", "A5", "A6"],
+                    "BMW": ["1 series", "2 series", "3 series", "4 series", "5 series", "6 series", "7 series", "8 series"],
+                    "Tesla": ["Model 3", "Model S", "Model X", "Model Y"]
+    ]
+
+}
---
+++ ParkingControl/FixVehicleTableViewController.swift
@@ -0,0 +1,100 @@
+//
+//  FixVehicleTableViewController.swift
+//  ParkingControl
+//
+//  Created by Mikalaj Shuhno on 30.11.20.
+//
+
+import UIKit
+import MapKit
+import CoreData
+
+class FixVehicleTableViewController: UITableViewController, UITextFieldDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    
+    @IBOutlet weak var numberVehicleTF: UITextField!
+    @IBOutlet weak var numberVehicleSwitch: UISwitch!
+    @IBOutlet weak var brandVehicleTF: UITextField!
+    @IBOutlet weak var modelVehicleTF: UITextField!
+    @IBOutlet weak var placeOfInspection: MKMapView!
+    var vehiclePickerView: UIPickerView!
+    
+    @IBAction func fixVehicleAction(_ sender: Any) {
+    }
+    @IBAction func backgroundTapped(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func NVChangeState(_ sender: Any) {
+        changeSwitchState(sender: numberVehicleSwitch)
+    }
+    
+    
+    
+    
+    
+    private func changeSwitchState(sender: UISwitch) {
+        if (sender.isOn) == false {
+            numberVehicleTF.isEnabled = true
+        } else {
+            numberVehicleTF.isEnabled = false
+            numberVehicleTF.placeholder = "Номер отсутствует"
+        }
+    }
+    
+    private func setupTextFields() {
+        let toolbar = UIToolbar()
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(doneButtonTapped))
+        toolbar.setItems([flexSpace, doneButton], animated: true)
+        toolbar.sizeToFit()
+        
+        numberVehicleTF.inputAccessoryView = toolbar
+        
+    }
+    
+    @objc func doneButtonTapped() {
+        view.endEditing(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTextFields()
+        
+        vehiclePickerView = UIPickerView()
+        
+        vehiclePickerView.dataSource = self
+        vehiclePickerView.delegate = self
+        
+        brandVehicleTF.inputView = vehiclePickerView
+        brandVehicleTF.text = vehiclePickerViewValues [0]
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // clear first responder after tap reognizer and dismiss keyboard
+        view.endEditing(true)
+    }
+    
+    // MARK: - pickerView
+    
+    let vehiclePickerViewValues = ["Test 1", "Test 2", "Test 3", "Test 4", "Test 5"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return vehiclePickerViewValues.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return vehiclePickerViewValues[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        brandVehicleTF.text = vehiclePickerViewValues[row]
+        self.view.endEditing(true)
+    }
+}
+
