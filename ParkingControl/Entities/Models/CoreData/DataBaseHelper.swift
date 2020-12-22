4f8225978061125e4db685df275154904cd8849f
Mikalaj <shuhno@yahoo.com>
2020-12-22 17:36:59 +0300

    add entities

---
+++ ParkingControl/Entities/Models/CoreData/DataBaseHelper.swift
@@ -0,0 +1,19 @@
+//
+//  DataBaseHelper.swift
+//  ParkingControl
+//
+//  Created by Mikalaj Shuhno on 22.12.20.
+//
+
+import UIKit
+import CoreData
+
+class DataBaseHelper {
+    
+    static let shareInstance = DataBaseHelper()
+    
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    func save
+}
